2fe9952734cfbd3a9108350badd70fc8591f7e81
David Flores <Dflores5200@gmail.com>
2022-08-08 20:49:10 -0500

    Update Rfile-for-data.R

--- Data/Rfile-for-data.R
+++ Data/Rfile-for-data.R
@@ -1,1 +1,4 @@
+#import data
 esportsearningsplayers <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/E-Sport-Final/highest_earning_players.csv')
+esportsearningsteams <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/E-Sport-Final/highest_earning_teams.csv')
+esportscountry <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/E-Sport-Final/country-and-continent-codes-list.csv')
