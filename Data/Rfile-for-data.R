e7f7b3df89e7286c5d488df6d7e6bab59b63caa3
David Flores <Dflores5200@gmail.com>
2022-08-09 23:09:56 -0500

    Create Rfile-for-data.R

---
+++ Data/Rfile-for-data.R
@@ -0,0 +1,11 @@
+#import data
+esportsearningsplayers <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/E-Sport-Final/highest_earning_players.csv')
+esportsearningsteams <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/E-Sport-Final/highest_earning_teams.csv')
+esportscountry <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/E-Sport-Final/country-and-continent-codes-list.csv')
+#showcase what game are currently competitive esports games
+unique(esportsearningsteams$Game)
+# [1] "Overwatch"                        "Starcraft II"                     "League of Legends"                "Fortnite"                        
+#[5] "Counter-Strike: Global Offensive"    "Dota 2"                           "PUBG"                             "Heroes of the Storm"             
+#[9] "Hearthstone"      
+#showcase the teams 
+unique(esportsearningsteams$TeamName)
