db59f0b0a32577c102e744a521a40a4a30e527eb
David Flores <Dflores5200@gmail.com>
2022-08-11 18:26:23 -0500

    Update Rfile-for-data.R

--- Code/Rfile-for-data.R
+++ Code/Rfile-for-data.R
@@ -1,7 +1,7 @@
 #import data
-esportsearningsplayers <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/E-Sport-Final/highest_earning_players.csv')
-esportsearningsteams <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/E-Sport-Final/highest_earning_teams.csv')
-esportscountry <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/E-Sport-Final/country-and-continent-codes-list.csv')
+esportsearningsplayers <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/Data/highest_earning_players.csv')
+esportsearningsteams <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/Data/highest_earning_teams.csv')
+esportscountry <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/Data/country-and-continent-codes-list.csv')
 #showcase what game are currently competitive esports games
 unique(esportsearningsteams$Game)
 # [1] "Overwatch"                        "Starcraft II"                     "League of Legends"                "Fortnite"                        
@@ -25,5 +25,5 @@ library(ggplot2)
 ggplot(esportsearningsteams, aes(x = Game)) + geom_bar()
 
 #import data with no national teams. Used SQL to remove national teams from the mix of teams
-Nonationalteams <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/Code/NoNational.csv')
+Nonationalteams <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/Data/NoNational.csv')
 
