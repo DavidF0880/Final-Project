449b71717e104b41cab063fc65b85764590f683a
David Flores <Dflores5200@gmail.com>
2022-08-11 17:48:13 -0500

    moving files

--- Code/Rfile-for-data.R
+++ Code/Rfile-for-data.R
@@ -6,4 +6,24 @@ esportscountry <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-
 unique(esportsearningsteams$Game)
 # [1] "Overwatch"                        "Starcraft II"                     "League of Legends"                "Fortnite"                        
 #[5] "Counter-Strike: Global Offensive"    "Dota 2"                           "PUBG"                             "Heroes of the Storm"             
-#[9] "Hearthstone"      
+#[9] "Hearthstone" 
+
+#Data Wrangling
+
+#showcase the teams 
+unique(esportsearningsteams$TeamName)
+
+#Questions to answer: How influential are League of Legends teams compared to the other games in E-Sports using TotalUSDPrize. 
+#Use ANOVA testing with t-test? Maybe compare to another game to see the difference in influence in the industry.
+#
+
+#independent variable : tournaments.
+#dependent variable : teams/TotalUSDPrize (2 dependent variables)
+
+#MAKE A HISTOGRAM
+library(ggplot2)
+ggplot(esportsearningsteams, aes(x = Game)) + geom_bar()
+
+#import data with no national teams. Used SQL to remove national teams from the mix of teams
+Nonationalteams <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/Code/NoNational.csv')
+
