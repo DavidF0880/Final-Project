b1b1d0f279527b40dce050b2383afdcf127ae1c0
David Flores <Dflores5200@gmail.com>
2022-08-11 22:27:59 -0500

    Update Rfile-for-data.R

--- Code/Rfile-for-data.R
+++ Code/Rfile-for-data.R
@@ -13,12 +13,17 @@ unique(esportsearningsteams$Game)
 #showcase the teams 
 unique(esportsearningsteams$TeamName)
 
-#Questions to answer: How influential are League of Legends teams compared to the other games in E-Sports using TotalUSDPrize. 
-#Use ANOVA testing with t-test? Maybe compare to another game to see the difference in influence in the industry.
-#
+#Questions to answer: It is believed that League of legends holds the most influence in E-Sports. Believed that it holds 19% of influence in the industry. Is this true?
+#Use Chi_square testing with t-test? Maybe compare to another game to see the difference in influence in the industry.
+library(dplyr)
+Nonationalteams %>% group_by(Game) %>% summarize(count=n())
 
-#independent variable : tournaments.
-#dependent variable : teams/TotalUSDPrize (2 dependent variables)
+#Run analysis
+observed = c(100,99,97,36,64,71,100,76,91,99)
+expected = c(.19, .09, .09, .09, .09, .09, .09, .09, .09, .09)
+
+chisq.test(x = observed, p = expected) 
+#p value is <0.05 meaning that the influence of League of Legends is in fact not 19%
 
 #MAKE A HISTOGRAM
 library(ggplot2)
