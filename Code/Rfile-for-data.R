5749e69a9631af2f0710d35cccf5a5ad50076c7a
David Flores <Dflores5200@gmail.com>
2022-08-28 00:35:41 -0500

    Update Rfile-for-data.R

--- Code/Rfile-for-data.R
+++ Code/Rfile-for-data.R
@@ -16,6 +16,7 @@ unique(esportsearningsteams$TeamName)
 
 class(esportsearningsteams$Game)
 
+#-----------------------------------------------------------------------------------------------------------------------------------------
 
 #Questions to answer: It is believed that League of legends holds the most influence in E-Sports. Believed that it holds 19% of influence in the industry. Is this true?
 #Use Chi_square testing with t-test? Maybe compare to another game to see the difference in influence in the industry.
@@ -64,28 +65,75 @@ NonationalANOVA <- aov(Nonationalteams$GameR ~ Nonationalteams$TotalUSDPrize)
 
 summary(NonationalANOVA)
 
+#run a MANCOVA analysis
 
+#ensure Variables are Numeric
+str(Nonationalteams$GameR)
+str(Nonationalteams$TotalUSDPrize)
+#Subsetting
+keeps <- c("GameR", "TotalUSDPrize")
+Nonationalteams1 <- Nonationalteams[keeps]
+
+#format as a Matrix
+Nonationalteams2 <- as.matrix(Nonationalteams1)
+
+#Test Assumptions
+#Load Libaries
+library(mvnormtest)
+#drop any missing values 
+Nonationalteams3 <- na.omit(Nonationalteams2)
+
+#Multivariate Normality
+mshapiro.test(t(Nonationalteams3))
+#It does not meat expectations since p value is >.05
 
 #MAKE A HISTOGRAM
 library(ggplot2)
 ggplot(esportsearningsteams, aes(x = Game)) + geom_bar()
+#----------------------------------------------------------------------------------------------------------------------------------------- 
+
 
 #import data with no national teams. Used SQL to remove national teams from the mix of teams
 Nonationalteams <- read.csv('C:/Users/David/Documents/GitHub/Final-Project/Final-Project/Data/NoNational.csv')
 
-#see the average prize money from genre
-Average_Prize <- Nonationalteams %>% group_by(Genre) %>% summarize(ave.prize = mean(TotalUSDPrize)) 
+#see the average prize money and tournaments from genre
+Average_Prize <- Nonationalteams %>% group_by(Genre) %>% summarize(ave.prize = mean(TotalUSDPrize), ave.tournament = mean(TotalTournaments))
 #run an anova 
 
-
+#-----------------------------------------------------------------------------------------------------------------------------------------
+#How do top earning players impact the influence of game or genre based on TotalUSDPrize
 #data wrangle for esportsearningsplayers
 #remove data that is not needed
 esportsplayers <- select(esportsearningsplayers, CurrentHandle, TotalUSDPrize, Game, Genre)
+#Top Earners from each game:
+OW <- esportsplayers %>% filter(Game == "Overwatch") #Rascal
+SC <- esportsplayers %>% filter(Game == "Starcraft II") #Serral
+LOL <- esportsplayers %>% filter(Game == "League of Legends") #Faker
+Fort <- esportsplayers %>% filter(Game == "Fortnite") #Bugha
+CSGO <- esportsplayers %>% filter(Game == "Counter-Strike: Global Offensive") #dupreeh
+Dota <- esportsplayers %>% filter(Game == "Dota 2") #N0tail
+PUBG <- esportsplayers %>% filter(Game == "PUBG") #Loki
+HOTS <- esportsplayers %>% filter(Game == "Heroes of the Storm") #KyoCha
+Hearth <- esportsplayers %>% filter(Game == "Hearthstone") #Thijs
+ 
+#Now that we have identified the top earners, put them in their individual data set
+Topearners <- c("Rascal", "Serral", "Faker", "Bugha", "Dupreeh", "N0tail", "Loki", "KyoCha", "Thijs")
+
+Topearners2 <- mutate(esportsplayers, CurrentHandle = Topearners)
+Topearners1 <- data.frame(esportsplayers)
+  
+
+
 #see total earning by game
-Gamesearnings <- esportsplayers %>% group_by(Game) %>% summarize(ave.prize = mean(TotalUSDPrize))
+esportsplayers[c(1)]
+
+#-----------------------------------------------------------------------------------------------------------------------------------------
 
 #Possible Question
 #how does Total tournament impact TotalUSDPrize for games using "Genre"
+
+
+
 
 #compare teams using total tournament and totalusdprize
 #use group_by
